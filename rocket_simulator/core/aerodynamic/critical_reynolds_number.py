ca18ab6bacc4bd2ed6cff4a9d335bb14210544c8
JLucasAlencar <alencarj2021@gmail.com>
2022-02-08 21:15:08 -0300

    Update critical_reynolds_number.py

--- rocket_simulator/core/aerodynamic/critical_reynolds_number.py
+++ rocket_simulator/core/aerodynamic/critical_reynolds_number.py
@@ -1,2 +1,4 @@
 def critical_reynolds_number(material_roughness: float, rocket_length: float) -> float:
     return 51 * (material_roughness / rocket_length) ** -1.039
+
+#An average surface roughness for a regular paint is 60 micrometers.
