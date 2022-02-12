6adf371a310e15cad83b557af80c92d8670ef431
Marina Sales <97768491+mwrsalles@users.noreply.github.com>
2022-02-11 22:51:34 -0300

    Update burn_area.py

--- rocket_simulator/core/propulsion/burn_area.py
+++ rocket_simulator/core/propulsion/burn_area.py
@@ -1,7 +1,17 @@
+"""
+@author marina
+@email [marinasalesr@gmail.com]
+@create date 2022-02-11 22:12:23
+""" 
+
 from math import pi 
 
 def burn_area(d):
-  #d: intern diameter (user's input)
-  #Ab: burn area of the propellant grain 
+  '''burn_area 
+    Args:
+        d: intern diameter (user's input) 
+    Returns:
+        Ab: burn area of the propellant grain
+    '''    
   Ab = pi*(d**2)
   return Ab
