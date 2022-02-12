bac1f43e82f7ef1ff3a4ef651ac6ae0c434732bc
Marina Sales <97768491+mwrsalles@users.noreply.github.com>
2022-02-11 22:56:53 -0300

    Update throath_diameter_interval.py

--- rocket_simulator/core/propulsion/throath_diameter_interval.py
+++ rocket_simulator/core/propulsion/throath_diameter_interval.py
@@ -1,10 +1,21 @@
+"""
+@author marina
+@email [marinasalesr@gmail.com]
+@create date 2022-02-11 22:48:31
+""" 
 from math import pi
 
 def throath_diameter_interval(Ap):
-  #Ap: area of combustion products flow
-  #d_max: maximum diameter 
-  #d_min: minimum diameter
-  #d_: medium diameter
+  '''throath_diameter_interval
+    Args:
+        Ap: area of combustion products flow
+        
+    Intermidiary:
+      d_max: maximum diameter 
+      d_min: minimum diameter
+    Returns:
+        d_: medium diameter
+    '''    
   d_max = (2*Ap/pi)**(1/2)
   d_min = (4*Ap/3*pi)**(1/2)
   d_ = (d_max + d_min)/2
