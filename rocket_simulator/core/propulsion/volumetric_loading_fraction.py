4aaf28fd91464122a4bb53d4bc177da04ae80c0f
Marina Sales <97768491+mwrsalles@users.noreply.github.com>
2022-02-11 23:04:56 -0300

    Update volumetric_loading_fraction.py

--- rocket_simulator/core/propulsion/volumetric_loading_fraction.py
+++ rocket_simulator/core/propulsion/volumetric_loading_fraction.py
@@ -1,6 +1,16 @@
+"""
+@author marina
+@email [marinasalesr@gmail.com]
+@create date 2022-02-11 22:57:04
+""" 
+
 def volumetric_loading_fraction(Vg, Vc):
-  #Vc: combustion chamber volume
-  #Vg: propellant's volume
-  #Vl: volumetric loading fraction
+  '''volumetric_loading_fraction
+    Args:
+        Vg: propellant's volume
+        Vc: combustion chamber volume
+    Returns:
+        Vl: volumetric loading fraction
+    '''    
   Vl = Vg/Vc
   return Vl
