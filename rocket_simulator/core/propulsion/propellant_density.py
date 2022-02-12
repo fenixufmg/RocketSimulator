1a6260f9ab9963d6a8e126f8146444a813331353
Marina Sales <97768491+mwrsalles@users.noreply.github.com>
2022-02-11 22:50:28 -0300

    Update propellant_density.py

--- rocket_simulator/core/propulsion/propellant_density.py
+++ rocket_simulator/core/propulsion/propellant_density.py
@@ -1,6 +1,16 @@
+"""
+@author marina
+@email [marinasalesr@gmail.com]
+@create date 2022-02-11 22:23:58
+""" 
+
 def propellant_density(mg, Vg):
-  #mg: propellant's mass (users' input)
-  #Vg: volume = previously calculated
-  #rop: propellant's density
+  '''burn_area 
+    Args:
+        mg: propellant's mass (users' input) 
+        Vg: volume = previously calculated
+    Returns:
+        rop: propellant's density
+    '''    
   rop = mg/Vg
   return rop
