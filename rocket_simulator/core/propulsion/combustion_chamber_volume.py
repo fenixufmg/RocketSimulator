43eea6c2dc88acc91d4b221f700f10a702aaa405
Marina Sales <97768491+mwrsalles@users.noreply.github.com>
2022-02-11 22:31:36 -0300

    Update combustion_chamber_volume.py

--- rocket_simulator/core/propulsion/combustion_chamber_volume.py
+++ rocket_simulator/core/propulsion/combustion_chamber_volume.py
@@ -3,6 +3,7 @@ from math import pi
 def combustion_chamber_volume(Dc, Lc):
   #Dc: combustion chamber diameter 
   #Lc: combustion chamber lenght
+  #Vc: combustion chamber volume
   Vc = (pi/4)*(Dc**2)*Lc
   return Vc
   
