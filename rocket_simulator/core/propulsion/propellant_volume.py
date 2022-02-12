79cdd91b71bcfc8957664f0835695173c7711ee4
Marina Sales <97768491+mwrsalles@users.noreply.github.com>
2022-02-11 22:54:00 -0300

    Update propellant_volume.py

--- rocket_simulator/core/propulsion/propellant_volume.py
+++ rocket_simulator/core/propulsion/propellant_volume.py
@@ -1,10 +1,20 @@
+"""
+@author marina
+@email [marinasalesr@gmail.com]
+@create date 2022-02-11 22:36:47
+""" 
+
 from math import pi
 
 def propellant_volume(D, d, L, N):
-  #D: external diameter (users' input)
-  #d: internal diameter (users' input)
-  #L: segment's lenght (users' input)
-  #N: number of segments (users' input)
-  #Vg: propellant's volume
+  '''propellant_volume 
+    Args:
+        D: external diameter (users' input)
+        d: internal diameter (users' input)
+        L: segment's lenght (users' input)
+        N: number of segments (users' input)
+    Returns:
+        Vg: propellant's volume
+    '''    
   Vg = (math.pi/4)*(D**2 - d**2)*L*N
   return Vg 
