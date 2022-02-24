49b29ddb93b2e840980f524f576612f571cd44cb
JLucasAlencar <alencarj2021@gmail.com>
2022-02-24 16:17:25 -0300

    Create mean_aerodynamic_chord.py

---
+++ rocket_simulator/core/aerodynamic/mean_aerodynamic_chord.py
@@ -0,0 +1,12 @@
+"""
+/**
+ * @author [author]
+ * @email [example@mail.com]
+ * @create date 2022-02-24 14:56:52
+ * @modify date 2022-02-24 14:56:52
+ * @desc [description]
+ */
+ """
+
+ def mean_aerodynamic_chord_length(spanwise: float, chord_length: float, fin_area: float):
+     
