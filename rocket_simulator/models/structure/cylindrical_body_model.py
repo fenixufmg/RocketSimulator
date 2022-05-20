bd3ff6050ff6fff79431af795cfef2fdb679032a
diego <dmgp1302@gmail.com>
2022-05-20 17:59:53 -0300

    Including method to calculate volume and mass for cylindrical body model

--- rocket_simulator/models/structure/cylindrical_body_model.py
+++ rocket_simulator/models/structure/cylindrical_body_model.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+from math import pi
 from core.physics.body.rigid_body import RigidBody
 from other.material_model import MaterialModel
 from models.structure.abstract_model import AbstractModel
@@ -11,3 +12,13 @@ class CylindricalBodyModel(AbstractModel):
         self.__thickness = thickness
         self.__material = material
         super().__init__()
+
+    def calculateVolume(self) -> float:
+        inner_diameter = self.__diameter -2*self.__thickness
+        volume = pi*self.__height*((self.__diameter/2)^2-(inner_diameter/2)^2)
+        return volume
+    
+    def calculateMass(self)-> float:
+        mass = self.__material.density*self.calculateVolume
+        return mass
+
