b30a4c271087dc82c6b74a96b641d2d095ec4429
diego <dmgp1302@gmail.com>
2022-06-24 21:49:23 -0300

    Finishing cylindrical body model (need to check if cp is correct, but assumed as same as local cg)

--- rocket_simulator/models/structure/cylindrical_body_model.py
+++ rocket_simulator/models/structure/cylindrical_body_model.py
@@ -32,8 +32,10 @@ class CylindricalBodyModel(AbstractModel):
         mass = self.material.density * self.calculateVolume
         return mass
 
-    def calculateMomentOfInertia(self, distance_to_cg: float) -> float:
-        raise NotImplementedError("Function not implemented")  # IMPLEMENTAR ESTA FUNÇÃO FUTURAMENTE
+    def calculateMomentOfInertia(self, distance_to_cg: float) -> float: # https://en.wikipedia.org/wiki/List_of_moments_of_inertia
+        mass = self.calculateMass
+        Ixx = 1/12*mass*(3*( (self.diameter^2)/4 +((self.diameter-2*self.thickness)^2)/4 )+self.height^2)
+        return Ixx
 
     def calculateCg(self) -> Vector:  # Feito amigo
         # Primeiro pega o vetor de altura da peça, a sua direção varia com a orientação do foguete, mas o módulo é
@@ -42,8 +44,14 @@ class CylindricalBodyModel(AbstractModel):
         cg_local = height * 0.5  # produto por escalar
         return self.toGroundCoordinates(cg_local)
 
-    def calculateCp(self) -> Vector:
-        raise NotImplementedError("Function not implemented")  # IMPLEMENTAR ESTA FUNÇÃO FUTURAMENTE
+    def calculateCp(self) -> Vector: # CONFERIR DEPOIS
+        height = self.getTipToBaseDistance()
+        cg_local = height * 0.5  # produto por escalar
+        cp_local = cg_local
+        return self.toGroundCoordinates(cp_local)
+
 
     def createDelimitationPoints(self) -> List[Vector]:
-        raise NotImplementedError("Function not implemented")  # IMPLEMENTAR ESTA FUNÇÃO FUTURAMENTE
+        upper_delimitation = Vector(0, 0, self.height)
+        lower_delimitation = Vector(0, 0, 0)
+        return [upper_delimitation, lower_delimitation]
