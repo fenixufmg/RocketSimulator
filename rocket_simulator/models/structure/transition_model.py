ed421e612f83a8e4ab436c9811b0b31cfd18f04f
diego <dmgp1302@gmail.com>
2022-06-27 17:16:30 -0300

    Finishing trasition model moment of inertia, fixing Cp and Cg basing on referentials

--- rocket_simulator/models/structure/transition_model.py
+++ rocket_simulator/models/structure/transition_model.py
@@ -44,26 +44,38 @@ class TransitionModel(AbstractModel):
         mass = self.__material.density * self.calculateVolume
         return mass
 
-    def calculateMomentOfInertia(self, distance_to_cg: float) -> float: # https://en.wikipedia.org/wiki/List_of_moments_of_inertia
-        raise NotImplementedError("Function not implemented")
+    def calculateMomentOfInertia(self, distance_to_cg: float) -> float: # https://media.cheggcdn.com/media/51d/51d19df6-2a92-4a5f-bdb2-275f8c7d8f0c/php43DZ1T.png based on last example
+        # from https://en.wikipedia.org/wiki/List_of_moments_of_inertia and parallel axis theorem
+        top_inner_radius = (self.__top_diameter - 2 * self.__thickness)/2
+        bottom_inner_radius = (self.__bottom_diameter - 2 * self.__thickness)/2
+        bottom_radius = self.__bottom_diameter/2
+        top_radius = self.__top_diameter/2
+        Iyy = 3*self.calculateMass*((bottom_radius^5+bottom_inner_radius^5)-(top_radius^5+top_inner_radius^5))/(
+            20*((bottom_radius^3+bottom_inner_radius^3)-(top_radius^3+top_inner_radius^3)))
+        return Iyy
 
     def calculateCg(self) -> Vector:  # aproximando por um tronco cheio https://www.passeidireto.com/pergunta/17392987/como-achar-um-centro-de-gravidade-em-um-tronco-de-cone 
+
+        bottom_radius = self.__bottom_diameter/2
+        top_radius = self.__top_diameter/2
+
         height = self.getTipToBaseDistance()
 
         if self.__bottom_diameter < self.__top_diameter:  # bottom part is thinner
-            z = Vector(0,0, self.__height/4*((self.__top_diameter^2 +2*self.__top_diameter*self.__bottom_diameter + 3*self.__bottom_diameter^2)/(
-                self.__top_diameter^2 + self.__top_diameter*self.__bottom_diameter + self.__bottom_diameter^2)) )
-            cg_local = height - z 
+            cg_local = self.__height -self.__height/4*((top_radius^2 +2*top_radius*bottom_radius + 3*bottom_radius^2)/(
+                top_radius^2 + top_radius*bottom_radius + bottom_radius^2))
             
         else:
-            z = Vector(0,0, self.__height/4*((self.__bottom_diameter^2 +2*self.__bottom_diameter*self.__top_diameter + 3*self.__top_diameter^2)/(
-                self.__bottom_diameter^2 + self.__bottom_diameter*self.__top_diameter + self.__top_diameter^2)) )
-            cg_local = height - z 
+            cg_local = self.__height -self.__height/4*((bottom_radius^2 +2*bottom_radius*top_radius + 3*top_radius^2)/(
+                bottom_radius^2 + bottom_radius*top_radius + top_radius^2))
             
-        return self.toGroundCoordinates(cg_local)
+        cg = self.getTipToBaseDistance().unitVector() * cg_local
 
-    def calculateCp(self) -> Vector: 
-        raise NotImplementedError("Function not implemented")
+        return self.toGroundCoordinates(cg)
+
+    def calculateCp(self) -> Vector: # Reference from donwloaded files
+        Cp = self.getTipToBaseDistance() * (1/3)*(1 + (1-self.__top_diameter/self.__bottom_diameter)/(1-(self.__top_diameter/self.__bottom_diameter)^2))
+        return self.toGroundCoordinates(Cp)
 
     def createDelimitationPoints(self) -> List[Vector]:
         upper_delimitation = Vector(0, 0, self.height)
