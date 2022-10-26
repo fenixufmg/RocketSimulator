39bfd2ba4b2d728ee9d101f46f8c001c0c9f647d
Marina Sales <97768491+mwrsalles@users.noreply.github.com>
2022-10-26 12:38:06 -0300

    editing documentation

--- rocket_simulator/core/propulsion/nozzle_exit_area.py
+++ rocket_simulator/core/propulsion/nozzle_exit_area.py
@@ -4,14 +4,13 @@
  * @email lerytripgo@gmail.com
  * @create date 2022-02-23 16:54:57
  * @modify date 2022-02-23 16:54:57
- * @desc [description]
  */
  """
 import math
 
 def nozzle_exit_area(de):
     """
-    This function returns the nozzle exit area.
+    This function returns the nozzle exit area. It's based on a circle's area using the nozzle's exit's diameter (previously calculated).
 
     Args:
         de (num): diameter of nozzle's exit.
