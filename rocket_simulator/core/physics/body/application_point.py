6f7a5483b0472606b3ec6c395ca058f1c1750478
Marina Sales <97768491+mwrsalles@users.noreply.github.com>
2022-11-19 22:43:21 -0300

    Update application_point.py

--- rocket_simulator/core/physics/body/application_point.py
+++ rocket_simulator/core/physics/body/application_point.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class ApplicationPoint(Enum):
-    """Enum que representa o ponto de aplicação de uma força.
+    """Enum which represents the application point of a force.
     """
     CG = 1
     CP = 2
